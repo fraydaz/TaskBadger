bc439503b8c2b8d9880f889183cd62c5b657cfa6
Frayda <fraydaz@gmail.com>
2018-10-20 15:40:24 -0400

    restored changes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -25,37 +25,22 @@ MainWindow::~MainWindow()
 
 void MainWindow::setTreeView()
 {
-    QSqlDatabase localdb = QSqlDatabase::database("MyDB");
-    this->model = new QSqlQueryModel();
-
     QString sql = "SELECT name AS 'My Projects' FROM category ORDER BY id ASC;";
-    QSqlQuery* query = new QSqlQuery(localdb);
-    query->prepare(sql);
-
-    if(!query->exec())
-        qWarning() << "ERROR: " << query->lastError();
-    else
-        qDebug () << "Query Successful: " << query->lastQuery();
-     model->setQuery(*query);
-
+    QSqlQuery query = DB->sqlSelect(sql);
+    this->model = new QSqlQueryModel();
+    model->setQuery(query);
     ui->treeView->setModel(model);
 }
 void MainWindow::setTableView()
-{
-    QSqlDatabase localdb = QSqlDatabase::database("MyDB");
-    this->model = new QSqlQueryModel();
+{  
     QString sql = "SELECT id AS 'Project #', name AS Project, "
                   "priority AS Priority, statusID AS Status, "
                   "due_date AS 'Due Date' FROM project ORDER BY id ASC;";
-    QSqlQuery* query = new QSqlQuery(localdb);
-    query->prepare(sql);
-
-    if(!query->exec())
-        qWarning() << "ERROR: " << query->lastError();
-    else
-        qDebug () << "Query Successful: " << query->lastQuery();
-    model->setQuery(*query);
-
+    QSqlQuery query = DB->sqlSelect(sql);
+    this->model = new QSqlQueryModel();
+    model->setQuery(query);
+    ui->tableView->resizeColumnsToContents();
+    ui->tableView->setSortingEnabled(true);
     ui->tableView->setModel(model);
 }
 void MainWindow::on_createProject_clicked()
